9c7baae521e74f417d47b674facf8958dc249888
Fajarsya <f4j4rkokok@gmail.com>
2023-07-10 11:04:19 +0700

    new sql

--- sql/table.sql
+++ sql/table.sql
@@ -1,83 +1,88 @@
--- Tabel Customer
-CREATE TABLE Customer (
-  id SERIAL PRIMARY KEY,
-  full_name VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  NIK VARCHAR(255) NOT NULL,
-  noPhone VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
+-- Table User
+CREATE TABLE ms_user (
+  id VARCHAR(50) PRIMARY KEY,
+  username VARCHAR(50),
+  password VARCHAR(50),
+  role VARCHAR(7),
+  is_active BOOLEAN
+);
+
+-- Table ms_customer
+CREATE TABLE ms_customer (
+  id VARCHAR(50) PRIMARY KEY,
+  id_user VARCHAR(50) REFERENCES ms_user (id),
+  full_name VARCHAR(100) NOT NULL, 
+  NIK VARCHAR(15) NOT NULL,
+  noPhone VARCHAR(15) NOT NULL,
+  email VARCHAR(100) NOT NULL,
   address VARCHAR(255) NOT NULL,
-  create_at TIMESTAMP NOT NULL,
-  create_by VARCHAR(255) NOT NULL,
-  updated_at TIMESTAMP NOT NULL,
-  updated_by VARCHAR(255) NOT NULL
+  created_at TIMESTAMP NOT NULL,
+  created_by VARCHAR(50) NOT NULL,
+  updated_at TIMESTAMP NULL,
+  updated_by VARCHAR(50) NULL
 );
 
--- Tabel Member
-CREATE TABLE Member (
-  id SERIAL PRIMARY KEY,
-  id_user INT REFERENCES Customer (id),
-  type VARCHAR(255) NOT NULL,
+-- Table ms_member
+CREATE TABLE ms_member (
+  id VARCHAR(50) PRIMARY KEY,
+  id_customer VARCHAR(50) REFERENCES ms_customer (id),
+  type VARCHAR(50) NOT NULL,
   expire DATE NOT NULL,
-  create_at TIMESTAMP NOT NULL,
-  create_by VARCHAR(255) NOT NULL,
-  updated_at TIMESTAMP NOT NULL,
-  updated_by VARCHAR(255) NOT NULL
+  created_at TIMESTAMP NOT NULL,
+  created_by VARCHAR(50) NOT NULL,
+  updated_at TIMESTAMP NULL,
+  updated_by VARCHAR(50) NULL
 );
 
--- Tabel Vehicle
-CREATE TABLE Vehicle (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  type VARCHAR(255) NOT NULL,
-  release DATE NOT NULL,
+-- Table ms_vehicle
+CREATE TABLE ms_vehicle (
+  id VARCHAR(50) PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  type VARCHAR(50) NOT NULL,
+  identification_number INT NOT NULL,
+  machine_number INT NOT NULL,
+  release_date DATE NOT NULL,
   price DECIMAL NOT NULL,
-  status VARCHAR(255) NOT NULL
+  status VARCHAR(10) NOT NULL,
+  is_available BOOLEAN NOT NULL,
+  number_plate VARCHAR(10),
+  stnk VARCHAR(10),
+  no_bpkb VARCHAR(10)
 );
 
--- Tabel Detail_Vehicle
-CREATE TABLE Detail_Vehicle (
-  id SERIAL PRIMARY KEY,
-  id_vehicle INT REFERENCES Vehicle (id),
-  stock INT NOT NULL,
-  number_plate VARCHAR(255) NOT NULL,
-  stnk VARCHAR(255) NOT NULL,
-  no_bpkb VARCHAR(255) NOT NULL
-);
-
--- Tabel Cash
-CREATE TABLE Cash (
-  id SERIAL PRIMARY KEY,
-  id_vehicle INT REFERENCES Vehicle (id),
-  id_customer INT REFERENCES Customer (id),
+-- Table tx_cash
+CREATE TABLE tx_cash (
+  id VARCHAR(50) PRIMARY KEY,
+  id_vehicle VARCHAR(50) REFERENCES ms_vehicle (id),
+  id_customer VARCHAR(50) REFERENCES ms_customer (id),
   price DECIMAL NOT NULL,
   date_payment DATE NOT NULL,
-  create_at TIMESTAMP NOT NULL,
-  create_by VARCHAR(255) NOT NULL,
-  updated_at TIMESTAMP NOT NULL,
-  updated_by VARCHAR(255) NOT NULL
+  created_at TIMESTAMP NOT NULL,
+  created_by VARCHAR(50) NOT NULL,
+  updated_at TIMESTAMP NULL,
+  updated_by VARCHAR(50) NULL
 );
 
--- Tabel Credit
-CREATE TABLE Credit (
-  id SERIAL PRIMARY KEY,
-  id_vehicle INT REFERENCES Vehicle (id),
-  id_customer INT REFERENCES Customer (id),
+-- Table tx_credit
+CREATE TABLE tx_credit (
+  id VARCHAR(50) PRIMARY KEY,
+  id_vehicle VARCHAR(50) REFERENCES ms_vehicle (id),
+  id_customer VARCHAR(50) REFERENCES ms_customer (id),
   price DECIMAL NOT NULL,
   interest DECIMAL NOT NULL,
-  date_In DATE NOT NULL,
-  date_Out DATE NOT NULL,
-  create_at TIMESTAMP NOT NULL,
-  create_by VARCHAR(255) NOT NULL,
-  updated_at TIMESTAMP NOT NULL,
-  updated_by VARCHAR(255) NOT NULL
+  date_in DATE NOT NULL,
+  date_out DATE NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  created_by VARCHAR(50) NOT NULL,
+  updated_at TIMESTAMP NULL,
+  updated_by VARCHAR(50) NULL
 );
 
--- Tabel Installment_Credit
-CREATE TABLE Installment_Credit (
-  id SERIAL PRIMARY KEY,
-  id_vehicle INT REFERENCES Vehicle (id),
-  id_credit INT REFERENCES Credit (id),
+-- Table tx_installment_credit
+CREATE TABLE tx_installment_credit (
+  id VARCHAR(50) PRIMARY KEY,
+  id_vehicle VARCHAR(50) REFERENCES ms_vehicle (id),
+  id_credit VARCHAR(50) REFERENCES tx_credit (id),
   price DECIMAL NOT NULL,
   total_payment_now DECIMAL NOT NULL,
   date_payment DATE NOT NULL,
@@ -87,15 +92,15 @@ CREATE TABLE Installment_Credit (
   suspend BOOLEAN NOT NULL
 );
 
--- Tabel Rent
-CREATE TABLE Rent (
-  id SERIAL PRIMARY KEY,
-  id_vehicle INT REFERENCES Vehicle (id),
-  id_customer INT REFERENCES Customer (id),
+-- Table tx_rent
+CREATE TABLE tx_rent (
+  id VARCHAR(50) PRIMARY KEY,
+  id_vehicle VARCHAR(50) REFERENCES ms_vehicle (id),
+  id_customer VARCHAR(50) REFERENCES ms_customer (id),
   price DECIMAL NOT NULL,
-  date_In DATE NOT NULL,
-  date_Out DATE NOT NULL,
-  status VARCHAR(255) NOT NULL,
-  create_by VARCHAR(255) NOT NULL,
-  updated_by VARCHAR(255) NOT NULL
+  date_in DATE NOT NULL,
+  date_out DATE NOT NULL,
+  status VARCHAR(50) NOT NULL,
+  created_by VARCHAR(50) NOT NULL,
+  updated_by VARCHAR(50) NULL
 );
